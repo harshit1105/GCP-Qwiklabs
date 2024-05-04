212d6874696adbccc974b40b2f44bc9457efa90e
harshit1105 <harshitsan@gmail.com>
2024-05-04 10:25:06 +0530

    Code completed Successfully and tested

--- Network_Tiers_-_Optimizing_Network_Spend/main.tf
+++ Network_Tiers_-_Optimizing_Network_Spend/main.tf
@@ -1,4 +1,4 @@
-module "compute_instance" {
+module "compute_instance_1" {
   source       = "./modules/compute"
   machine_name = var.machine_name
   machine_type = var.machine_type
@@ -6,4 +6,14 @@ module "compute_instance" {
   my_region    = var.my_region
   my_zone      = var.my_zone
   network_tier = lookup(var.network_tier, var.machine_name, "STANDARD")
+}
+
+module "compute_instance_2" {
+  source       = "./modules/compute"
+  machine_name = "vm-standard"
+  machine_type = var.machine_type
+  my_project   = var.my_project
+  my_region    = var.my_region
+  my_zone      = var.my_zone
+  network_tier = lookup(var.network_tier, "vm-standard", "STANDARD")
 }
--- Network_Tiers_-_Optimizing_Network_Spend/terraform.tfvars
+++ Network_Tiers_-_Optimizing_Network_Spend/terraform.tfvars
@@ -1,5 +1,5 @@
-my_project   = "aesthetic-vent-418205" #Need to update this
-my_region    = "asia-south1"           #Need to update this
-my_zone      = "asia-south1-a"         #Need to update this
+my_project   = "" #Need to update this
+my_region    = ""           #Need to update this
+my_zone      = ""         #Need to update this
 machine_type = "e2-medium"
 machine_name = "vm-premium"
